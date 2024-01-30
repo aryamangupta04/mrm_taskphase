01306f209eac0f2b1422f3b8546b3876a7dc8aa2
aryamangupta04 <aryamangupta2004@gmail.com>
2024-01-30 18:18:52 +0530

    val-set-updated

--- MNIST_PYTORCH_MODEL/train.py
+++ MNIST_PYTORCH_MODEL/train.py
@@ -27,23 +27,7 @@ model=CNN(in_channels=input_channel,num_classes=num_classes).to(device)
 
 loss_func=nn.CrossEntropyLoss()
 optimizer=optim.Adam(model.parameters(),lr=learning_rate)
-
-for epoch in range(num_epochs):
-    epoch_loss=0.0
-    for data,targets in train_loader:
-        data=data.to(device=device)
-        targets=targets.to(device=device)
-        scores=model(data)
-        loss=loss_func(scores,targets)
-
-        optimizer.zero_grad()
-        loss.backward()
-
-        optimizer.step()
-        epoch_loss+=loss.item()
-    epoch_loss/=len(train_loader)
-    train_losses.append(epoch_loss)
-
+val_acc_list=[]
 def check_accuracy(loader,model):
     correct=0
     samples=0
@@ -60,12 +44,35 @@ def check_accuracy(loader,model):
 
     acc=(correct/samples)*100
     return acc
-torch.save(model.state_dict(), 'trained_model.pth')
+max_val=0
+for epoch in range(num_epochs):
+    epoch_loss=0.0
+    for data,targets in train_loader:
+        data=data.to(device=device)
+        targets=targets.to(device=device)
+        scores=model(data)
+        loss=loss_func(scores,targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        optimizer.step()
+        epoch_loss+=loss.item()
+    val_acc=  check_accuracy(val_loader,model)
+    val_acc_list.append(val_acc.item())
+    if(val_acc>max_val):  
+        max_val=val_acc
+        torch.save(model.state_dict(), 'trained_model.pth')
+    epoch_loss/=len(train_loader)
+    train_losses.append(epoch_loss)
+
+
+
 t_acc=check_accuracy(train_loader, model)
-val_acc=check_accuracy(val_loader,model)
+print(val_acc_list)
+print("best val accuracy is: ",max_val)
 
 print("the training accuracy is: ",t_acc.item())
-print("the validation accuracy is: ",val_acc.item())
 
 plt.plot(range(1,num_epochs+1),train_losses,label='Training Loss')
 plt.xlabel('Epoch')
